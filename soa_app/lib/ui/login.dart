1d82e9507f543adb5f37a266ce77c94e9d33b7df
gabrielmk666 <44278773+gabrielmk666@users.noreply.github.com>
2019-08-30 23:55:29 -0300

    Testando Tela Nova
    Fazendo o estilo de página

--- soa_app/lib/ui/login.dart
+++ soa_app/lib/ui/login.dart
@@ -11,20 +11,84 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-        backgroundColor: Colors.deepPurpleAccent,),
-        body:Center(
+      //appBar: AppBar(
+      //  title: Text("Login"),
+      // backgroundColor: Colors.deepPurpleAccent,),
+        body: Container(
+          //padding: EdgeInsets.all(10),
           child: Center(
             child: Column(
+              //crossAxisAlignment: CrossAxisAlignment.stretch,
+              //mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              RaisedButton(onPressed: () {Navigator.of(context).pushNamed(HomeView);},child: Text('Login'),),
-              RaisedButton(onPressed: () {Navigator.of(context).pushNamed(LoginPasswordView);},child: Text('Esqueceu s Senha'),),
-              RaisedButton(onPressed: () {Navigator.of(context).pushNamed(LoginRegisterView);},child: Text('Cadastrar'),)
-            ],
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height/2.5,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFFf45d27),
+                      Color(0xFFf5851f),
+                    ]
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)
+                  ),
+              ),
           ),
-          )
-        )
+          
+             Container(
+              
+                child: TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                style: new TextStyle(color: Colors.deepPurpleAccent, fontSize: 15),
+                decoration:  InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                    ),
+                  ),
+
+              TextFormField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                style: new TextStyle(color: Colors.deepPurpleAccent, fontSize: 15),
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                ),
+              ),
+            ),
+
+             Container (
+
+               RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(HomeView);
+                  },
+                  child: Text('Login'),
+                  ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(LoginPasswordView);
+                  },
+                  child: Text('Esqueceu s Senha'),
+                  ),
+              RaisedButton(
+                onPressed: () 
+                {Navigator.of(context).pushNamed(LoginRegisterView);
+                },
+                child: Text('Cadastrar'),
+                ),
+             ),
+            ],
+            ),
+          ),
+      ),
     );
   }
 }
