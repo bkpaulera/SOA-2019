eb21845118eb07be9e46586a081f559e8b477ce7
Paulo Cristovam <paulo.cristovam@kognit.com.br>
2019-09-06 10:28:13 -0300

    Adição do Card

--- soa_app/lib/ui/search/search.dart
+++ soa_app/lib/ui/search/search.dart
@@ -2,22 +2,39 @@
 import 'package:flutter/material.dart';
 import 'package:soa_app/ui/search/card.dart';
 
+List <CardAlert> CardsAlerts  = new List(10);
+
+
 class Search extends StatefulWidget {
   @override
   _SearchState createState() => _SearchState();
 }
 
+
+
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-     return MaterialApp(
+    return MaterialApp(
         title: 'Flutter Demo',
         home: 
         Scaffold(
-          appBar: AppBar(),
-          body:Center(
-                child: CardStudy(),
-            )
+          appBar: AppBar(
+            title: Text('Alerta'),
+            backgroundColor: Colors.deepPurpleAccent,
+            centerTitle: true,
+          ),
+          backgroundColor: Colors.white,
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){},
+            child: Icon(Icons.add),
+            backgroundColor: Colors.deepPurpleAccent,
+          ),
+          body: ListView.builder(padding: EdgeInsets.all(10.0),
+          itemCount: CardsAlerts.length,
+          itemBuilder: (context,index){
+            return CardStudy();
+          },
         )
     );
   }
