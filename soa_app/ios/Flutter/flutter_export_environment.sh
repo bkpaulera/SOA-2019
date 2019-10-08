a5813c10d6bef7f289f8ea5db0c1f4f628fbae73
paulokognit <51748311+paulokognit@users.noreply.github.com>
2019-10-08 14:00:13 -0300

    Update do Flutter

---
+++ soa_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\paulo.cristovam.kognit\Documents\GitHub\PeronalProjects\Flutter\SOA-2019\soa_app"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\Flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
